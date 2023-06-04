75ba59c8b38b0245cd6710d0a12a617ed512f610
Nue <phonehtetaung1993@gmail.com>
2023-06-04 15:04:18 +0630

    implemented global countdown and skip function

--- src/components/QuizCard.tsx
+++ src/components/QuizCard.tsx
@@ -4,11 +4,20 @@ import { QuizAns } from "../utils/QuizAns";
 import { plusScore } from "../features/user/userSlice";
 import quizs from "../data.json";
 import { finishGame, quitGame } from "../features/user/userSlice";
+import { QuizDataType } from "../typings/types";
+
+// const defaultRemainTime = {
+//   seconds: 30,
+//   minutes: 1,
+// };
 
 export const QuizCard = () => {
   const [index, setIndex] = useState(0);
-  const [countDown, setCountDown] = useState(5);
+  const [countDown, setCountDown] = useState(50);
+  // const [remainingTime, setRemainingTime] = useState(defaultRemainTime);
   const [showAnswer, setShowAnswer] = useState(false);
+  const [skippedQus, setSkippedQus] = useState<QuizDataType[]>([]);
+  const [hasSkipped, setHasSkipped] = useState<boolean>(false);
 
   const timeRef = useRef<null | number>(null);
   const user = useAppSelector((state) => state.user);
@@ -16,23 +25,38 @@ export const QuizCard = () => {
 
   // next event
   const handleNextClick = useCallback(() => {
-    timeRef.current && clearTimeout(timeRef.current);
+    setShowAnswer(false);
     if (index < quizs.length - 1) {
-      setCountDown(5);
       setIndex((pre) => pre + 1);
-      setShowAnswer(false);
+    } else {
+      setIndex(0);
+      if (skippedQus.length > 0) return setHasSkipped(true);
+      dispatch(finishGame());
+    }
+  }, [dispatch, index, skippedQus.length]);
+
+  const handleSkipNextClick = useCallback(() => {
+    setShowAnswer(false);
+    if (index < skippedQus.length - 1) {
+      setIndex((pre) => pre + 1);
     } else {
       setIndex(0);
       dispatch(finishGame());
     }
-  }, [dispatch, index]);
+  }, [dispatch, index, skippedQus.length]);
+
+  const handleSkipClick = () => {
+    setSkippedQus((pre) => [...pre, quizs[index]]);
+    setIndex((pre) => pre + 1);
+  };
+  console.log("skipped Qus: ", skippedQus);
 
   // start timer
   const startTimer = () => {
     timeRef.current = setTimeout(() => {
       if (countDown === 0) {
         timeRef.current && clearTimeout(timeRef.current);
-        handleNextClick();
+        dispatch(finishGame());
       } else {
         setCountDown(countDown - 1);
       }
@@ -42,16 +66,26 @@ export const QuizCard = () => {
   // check user answer and show true or false
   const checkAnswer = useCallback(
     (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-      timeRef.current && clearTimeout(timeRef.current);
       const userAnswer = e.currentTarget.id;
-      const correctAns = quizs[index].correct_answer;
-      if (userAnswer === correctAns) dispatch(plusScore());
+      // check skipped question or not
+      const correctAns = hasSkipped
+        ? skippedQus[index].correct_answer
+        : quizs[index].correct_answer;
+
+      if (userAnswer === correctAns) {
+        dispatch(plusScore());
+        timeRef.current && clearTimeout(timeRef.current);
+        setCountDown((pre) => pre + 3);
+      } else {
+        timeRef.current && clearTimeout(timeRef.current);
+        setCountDown((pre) => pre - 3);
+      }
       setShowAnswer(true);
     },
-    [dispatch, index]
+    [dispatch, hasSkipped, index, skippedQus]
   );
 
-  useEffect(startTimer, [countDown, handleNextClick]);
+  useEffect(startTimer, [countDown, dispatch, handleNextClick]);
 
   // quit game
   const quitGameHandler = () => {
@@ -59,8 +93,13 @@ export const QuizCard = () => {
   };
 
   const quizAnsList = useMemo(
-    () => QuizAns(quizs[index], showAnswer, checkAnswer),
-    [checkAnswer, index, showAnswer]
+    () =>
+      QuizAns(
+        hasSkipped ? skippedQus[index] : quizs[index],
+        showAnswer,
+        checkAnswer
+      ),
+    [checkAnswer, hasSkipped, index, showAnswer, skippedQus]
   );
 
   return (
@@ -73,21 +112,14 @@ export const QuizCard = () => {
           Quit
         </button>
       </div>
-      {/* question */}
-      <div className=" font-bold text-lg text-[#212f46] text-center">
-        {quizs[index].question}
-      </div>
-      {/* answers */}
-      <div className=" flex flex-wrap items-center justify-center my-4 sm:justify-between">
-        {/* <QuizAns
-          quiz={quizs[index]}
-          showAnswer={showAnswer}
-          checkAnswer={checkAnswer}
-        /> */}
-        {quizAnsList}
-      </div>
-      {/* next btn */}
-      <button className=" btn float-right" onClick={handleNextClick}>
+      {quizAnsList}
+      <button className=" btn" onClick={handleSkipClick}>
+        skip
+      </button>
+      <button
+        className=" btn float-right"
+        onClick={hasSkipped ? handleSkipNextClick : handleNextClick}
+      >
         Next
       </button>
     </div>
--- src/utils/QuizAns.tsx
+++ src/utils/QuizAns.tsx
@@ -1,39 +1,37 @@
 import { QuizDataType } from "../typings/types";
 
-// interface Props {
-//   quiz: QuizDataType;
-//   showAnswer: boolean;
-//   checkAnswer: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-// }
-
 export const QuizAns = (
   quiz: QuizDataType,
   showAnswer: boolean,
   checkAnswer: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 ) => {
-  console.log("hello from Quiz Ans");
   return (
     <>
-      {quiz.choices.map((ans) => {
-        const specialClassName = showAnswer
-          ? ans.id === quiz.correct_answer
-            ? "text-green-500"
-            : "text-red-600"
-          : "";
-        return (
-          <button
-            key={ans.id}
-            className={` btn_chose text-[#222] border border-[#222] rounded w-[20rem] my-2 px-3 py-2 ${specialClassName} cursor-pointer ${
-              showAnswer ? "" : "hover:bg-[#222] hover:text-[#fff]"
-            } transition-all duration-200 `}
-            id={ans.id}
-            onClick={checkAnswer}
-            disabled={showAnswer}
-          >
-            {ans.value}
-          </button>
-        );
-      })}
+      <div className=" font-bold text-lg text-[#212f46] text-center">
+        {quiz.question}
+      </div>
+      <div className=" flex flex-wrap items-center justify-center my-4 sm:justify-between">
+        {quiz.choices.map((ans) => {
+          const specialClassName = showAnswer
+            ? ans.id === quiz.correct_answer
+              ? "text-green-500"
+              : "text-red-600"
+            : "";
+          return (
+            <button
+              key={ans.id}
+              className={` btn_chose text-[#222] border border-[#222] rounded w-[20rem] my-2 px-3 py-2 ${specialClassName} cursor-pointer ${
+                showAnswer ? "" : "hover:bg-[#222] hover:text-[#fff]"
+              } transition-all duration-200 `}
+              id={ans.id}
+              onClick={checkAnswer}
+              disabled={showAnswer}
+            >
+              {ans.value}
+            </button>
+          );
+        })}
+      </div>
     </>
   );
 };
